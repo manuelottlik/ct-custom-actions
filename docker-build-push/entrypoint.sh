e820893f530313fdd10f3f99981ef6c9b45816a2
Manuel Ottlik <28919003+manuelottlik@users.noreply.github.com>
2021-02-15 22:41:44 +0100

    rename folder

--- docker-build-push/entrypoint.sh
+++ docker-build-push/entrypoint.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
-# makes the script existing once an error occours
 set -euo pipefail
 
 INPUT_IMAGE_NAME=${INPUT_IMAGE_NAME:-"${GITHUB_REPOSITORY#*/}"}
-
-# log into docker registry
-echo ${INPUT_REGISTRY_PASSWORD} | docker login -u ${INPUT_REGISTRY_USERNAME} --password-stdin ${INPUT_REGISTRY_URL}
-
 FULL_IMAGE_NAME=${INPUT_REGISTRY_URL}/${GITHUB_REPOSITORY}/${INPUT_IMAGE_NAME}:${INPUT_IMAGE_TAG}
 
+echo ${INPUT_REGISTRY_PASSWORD} | docker login -u ${INPUT_REGISTRY_USERNAME} --password-stdin ${INPUT_REGISTRY_URL}
 docker build --tag ${FULL_IMAGE_NAME} .
-
 docker push ${FULL_IMAGE_NAME}
